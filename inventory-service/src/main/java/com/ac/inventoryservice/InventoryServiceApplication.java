ef3e7a4e5f2f1c8cec4f0cb3029ac4ba6aef2337
Ilinca <ilinca-florinela.turcu@student.tuiasi.ro>
2022-09-05 11:09:27 +0300

    added DiscoveryServiceApplication and API Gateway application with routes configured for all existing services

--- inventory-service/src/main/java/com/ac/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/ac/inventoryservice/InventoryServiceApplication.java
@@ -5,16 +5,18 @@ import com.ac.inventoryservice.repository.InventoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
 
-	@Bean
+	/*@Bean
 	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
 		return args -> {
 			Inventory inventory = new Inventory();
@@ -28,6 +30,6 @@ public class InventoryServiceApplication {
 			inventoryRepository.save(inventory);
 			inventoryRepository.save(inventory1);
 		};
-	}
+	}*/
 
 }
